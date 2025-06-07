3d7e10aff900842753fb04f90264541c669c2663
Karthik Jayaraman <karthik.jay@outlook.com>
2025-06-07 15:20:54 +0800

    Project setup

---
+++ src/ChronoLogical.Api/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS for React development
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Vite default port
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("ReactApp");
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
